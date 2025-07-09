2f7e5ad6233da3a419adfc1409d8e3413aab6388
Nwachuya <106511230+Nwachuya@users.noreply.github.com>
2025-07-09 23:08:05 +0000

    Create simple React landing page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Deployment Test Successful!</h1>
+      <p>This page is being served from GitHub Codespaces & Coolify.</p>
+      <p>If you see this, the deployment pipeline is working correctly.</p>
+    </div>
+  )
+}
+
+export default App
